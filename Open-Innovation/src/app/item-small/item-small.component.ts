b124177b08f9563c0b073a469063434af18d47db
Aristeidis Symeonidis <aris.hexagon@gmail.com>
2018-10-18 22:54:28 -0400

    wider modal, added comment button and area

--- Open-Innovation/src/app/item-small/item-small.component.ts
+++ Open-Innovation/src/app/item-small/item-small.component.ts
@@ -27,7 +27,8 @@ export class ItemSmallComponent implements OnInit {
   }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    // const modalRef = this.modalService.open(content, { size: 'lg', backdrop: 'static' });
+    this.modalService.open(content, { size: 'lg', ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
